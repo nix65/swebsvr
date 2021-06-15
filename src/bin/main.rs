3dd54c5ad32d62bdcc0ae5be885314bb1609fd61
nix65 <john.holm1@mailfence.com>
2021-06-15 21:42:52 +0300

    a3

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,9 +3,9 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::thread;
-
 use std::time::Duration;
 
+use swebserv::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -13,25 +13,24 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        
-        thread::spawn(|| {
+
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
-
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
- 
+
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
-    } else if buffer.starts_with(sleep){
-        thread::sleep_ms(3000);
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_millis(3000));
         ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
@@ -44,8 +43,6 @@ fn handle_connection(mut stream: TcpStream) {
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 
-    
     //
     //println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-
 }
